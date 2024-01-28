0855645b0376a3f417c94858259ba1ecd6439966
nikonru <makaroff.nickita2013@yandex.ru>
2024-01-28 12:45:30 +0100

    Updated url

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='PyQt video player widget',
     package_data={'pyqt_graphics_video_item_video_player.style': ['button.css', 'slider.css'],
                   'pyqt_graphics_video_item_video_player.ico': ['pause.png', 'play.png', 'stop.png']},
-    url='https://github.com/yjg30737/pyqt-graphics-video-item-video-player.git',
+    url='https://github.com/nikonru/pyqt-graphics-video-item-video-player.git',
     install_requires=[
         'mutagen',
         'PyQt5>=5.8',
